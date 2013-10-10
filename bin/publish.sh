221c6f7855f3a0aa33fa2d5e2a91c029cd5ae0e8
Sean Clark Hess <seanhess@gmail.com>
2013-10-10 16:56:18 -0600

    publish works

--- bin/publish.sh
+++ bin/publish.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 # assumes data/index.json has been built
-rm -rf ./pages
-git clone . ./pages
-rm -rf ./pages/*
-rm -rf ./pages/*.gitignore
-mv data/index.json pages/index.json
-cd pages
-git checkout --orphan gh-pages
-git add index.json
+set -e
+git stash
+git checkout gh-pages
+git add data/index.json
 git commit -m 'index.json'
 git push origin gh-pages -f
+git checkout master
+git stash pop
